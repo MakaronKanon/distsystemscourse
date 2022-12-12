c39245e110f124296cec332d6da221181c5ec0e3
Marcus Jörgensson <marcusjorgensson@gmail.com>
2022-12-12 23:06:34 +0100

    Add chord

---
+++ chord/chord.go
@@ -0,0 +1,531 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha1"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"math/big"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	ipHost := flag.String("a", "required", "The IP address that the Chord client will bind to, as well as advertise to other nodes. Represented as an ASCII string (e.g., 128.8.126.63). Must be specified.")
+	portHost := flag.Int("p", -1, "The port that the Chord client will bind to and listen on. Represented as a base-10 integer. Must be specified.")
+
+	otherHost := flag.String("ja", "required", "The IP address of the machine running a Chord node. The Chord client will join this node’s ring. Represented as an ASCII string (e.g., 128.8.126.63). Must be specified if --jp is specified.")
+	otherPort := flag.Int("jp", -1, " The port that an existing Chord node is bound to and listening on. The Chord client will join this node’s ring. Represented as a base-10 integer. Must be specified if --ja is specified.")
+
+	timeBetweenStabilize := flag.Int("ts", -1, "The time in milliseconds between invocations of ‘stabilize’. Represented as a base-10 integer. Must be specified, with a value in the range of [1,60000].")
+	timeBetweenFixFingers := flag.Int("tff", -1, "The time in milliseconds between invocations of ‘fix fingers’. Represented as a base-10 integer. Must be specified, with a value in the range of [1,60000]")
+	timeBetweenCheckPredecessors := flag.Int("tcp", -1, "The time in milliseconds between invocations of ‘check predecessor’. Represented as a base-10 integer. Must be specified, with a value in the range of [1,60000]")
+	numSuccessors := flag.Int("r", -1, "The number of successors maintained by the Chord client. Represented as a base-10 integer. Must be specified, with a value in the range of [1,32]")
+	// overrideIdentifier := flag.String("i", "required", "The identifier (ID) assigned to the Chord client which will override the ID computed by the SHA1 sum of the client’s IP address and port number. Represented as a string of 40 characters matching [0-9a-fA-F]. Optional parameter.")
+
+	flag.Parse()
+
+	if *ipHost == "required" {
+		fmt.Println("Ip is required!")
+		os.Exit(1)
+	}
+	if *portHost == -1 {
+		fmt.Println("Port is required!")
+		os.Exit(1)
+	}
+	if *otherHost != "required" && *otherPort == -1 {
+		fmt.Println("Must set jp if ja is set.")
+		os.Exit(1)
+	}
+	if *otherHost == "required" && *otherPort != -1 {
+		fmt.Println("Must set ja if jp is set.")
+		os.Exit(1)
+	}
+	if *timeBetweenStabilize < 1 || *timeBetweenStabilize > 60000 {
+		fmt.Println("Time between stabilize '-ts' must be specified, in range [1,60000].")
+		os.Exit(1)
+	}
+	if *timeBetweenFixFingers < 1 || *timeBetweenFixFingers > 60000 {
+		fmt.Println("Time between fix fingers '-tff' must be specified, in range [1,60000].")
+		os.Exit(1)
+	}
+	if *timeBetweenCheckPredecessors < 1 || *timeBetweenCheckPredecessors > 60000 {
+		fmt.Println("Time between check predecessors '-tcp' must be specified, in range [1,60000].")
+		os.Exit(1)
+	}
+	if *numSuccessors < 1 || *numSuccessors > 32 {
+		fmt.Println("Number of successors '-r' must be specified, in range [1,32].")
+		os.Exit(1)
+	}
+
+	fmt.Println("Hello world", *ipHost, *portHost)
+
+	createRing(*ipHost, *portHost)
+	go listenForConnections(*ipHost, *portHost)
+
+	go stabilize(*timeBetweenStabilize)
+	// go fixFingers()
+	// go checkPredecessor()
+
+	// Continuosly read commands.
+	readCommands()
+
+}
+
+type node struct {
+	address string
+	id      id
+}
+
+func createNode(address string) node {
+	return node{
+		address: address,
+		id:      getId(address),
+	}
+}
+
+var successor node
+var currentNode node
+var predecessor *node
+
+func createRing(ip string, port int) {
+	predecessor = nil
+	successor = createNode(fmt.Sprintf("%s:%d", ip, port))
+	currentNode = successor
+}
+
+func joinRing() { // otherNode) {
+	// predecessor = nil
+	// successor = otherNode.findSuccessor(currentNodeId)
+}
+
+func stabilize(interval int) {
+	for {
+		x := nodeInterface.GetPredeccessor(successor)
+
+		fmt.Println("Stabilizing x is ", x)
+		if isBetween(x, currentNode, successor) {
+			successor = x
+		}
+		nodeInterface.Notify(successor)
+
+		time.Sleep(interval) // int(time.Second) * interval)
+	}
+}
+
+// Other node notify us of their existance.
+func notify(node node) {
+	if predecessor == nil {
+		// We don't have anything better as predecessor so use it.
+		predecessor = &node
+		return
+	}
+	if isBetween(node, *predecessor, currentNode) {
+		predecessor = &node
+	}
+
+}
+
+// Whether x is in range (y, z) in the circle.
+func isBetween(x node, y node, z node) bool {
+	return false
+}
+
+func fixFingers() {
+	for {
+		fmt.Println("Fixing fingers")
+		// text, _ := reader.ReadString('\n');
+
+		//
+
+		time.Sleep(time.Second)
+	}
+
+}
+
+func checkPredecessor() {
+	for {
+		fmt.Println("Checking predecessor")
+
+		// if *redecessor = nil {
+		// 	return
+		// }
+		// isAlive := predecessor.ping()
+		// if !isAlive {
+		// 	predecessor = nil
+		// }
+
+		// text, _ := reader.ReadString('\n');
+
+		//
+
+		time.Sleep(time.Second)
+	}
+
+}
+
+func readCommands() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Reading commands")
+		text, _ := reader.ReadString('\n')
+
+		inputs := strings.Split(text, " ")
+		fmt.Println("Inputs are ", inputs)
+		switch inputs[0] {
+		case "lookup":
+			lookupFile(inputs[1])
+		case "storeFile":
+			storeFile(inputs[1])
+		case "printState":
+			printState()
+		case "ping":
+			pingSuccessor()
+		}
+	}
+}
+
+type id = *big.Int
+
+type NodeInterface interface {
+	Ping(node node) bool
+	FindSuccessor(node node, id id) (bool, node)
+	GetPredeccessor(node node) node
+	Notify(node node)
+}
+
+type HttpNodeInterface struct{}
+
+func (h HttpNodeInterface) Ping(node node) bool {
+	resp, err := http.Get("http://" + node.address + "/ping")
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println("Error pinging : ", err.Error())
+		return false
+	}
+	return true
+}
+
+type FindSuccessorHttpResponse struct {
+	FoundSuccessor  bool   `json:"foundSuccessor"`
+	SuccessorAdress string `json:"successorAdress"`
+}
+
+func (h HttpNodeInterface) FindSuccessor(node node, id id) (bool, node) {
+	resp, err := http.Get("http://" + node.address + "/findSuccessor?id=" + id.String())
+	fmt.Println("Before parse response is ", resp.Body)
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println("Error finding successor : ", err.Error())
+
+		panic("Panic")
+	}
+	var parsedResp FindSuccessorHttpResponse
+	err = json.NewDecoder(resp.Body).Decode(&parsedResp)
+	fmt.Println("Parsed the response to ", parsedResp)
+	if err != nil {
+		panic(err)
+	}
+	return parsedResp.FoundSuccessor, createNode(parsedResp.SuccessorAdress)
+
+}
+
+type GetPredeccessorHttpResponse struct {
+	address string
+}
+
+func (HttpNodeInterface) GetPredeccessor(node node) node {
+	resp, err := http.Get("http://" + node.address + "/getPredeccessor")
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println("Error finding predeccessor : ", err.Error())
+		return createNode("bad")
+	}
+	fmt.Println("Before parse response is ", resp.Body)
+	var parsedResp GetPredeccessorHttpResponse
+	err = json.NewDecoder(resp.Body).Decode(&parsedResp)
+	fmt.Println("Parsed the response to ", parsedResp)
+	if err != nil {
+		panic(err)
+	}
+	return createNode(parsedResp.address)
+}
+
+func (HttpNodeInterface) Notify(node node) {
+	resp, err := http.Get("http://" + node.address + "/notify?address=" + currentNode.address)
+	fmt.Println("Before parse response is ", resp.Body)
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println("Error finding successor : ", err.Error())
+
+		panic("Panic")
+	}
+
+	// var parsedResp NotifyHttpResponse
+	// err = json.NewDecoder(resp.Body).Decode(&parsedResp)
+	// fmt.Println("Parsed the response to ", parsedResp)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// return parsedResp.FoundSuccessor, createNode(parsedResp.SuccessorAdress)
+
+}
+
+var nodeInterface = HttpNodeInterface{}
+
+func pingSuccessor() {
+	gotAnswer := nodeInterface.Ping(successor)
+	fmt.Println("Answer: ", gotAnswer)
+}
+
+func lookupFile(filename string) {
+	id := getId(filename)
+
+	owner := findSuccessorIteratively(id)
+
+	// file := nodeInterface.RequestFile(owner, filename)
+
+	// hej.txt -> oasethu134n5hntsoaehu
+	// Hash the filename to get the fileid.
+	// owner := findSuccessor(id)
+	// owner.requestFile(filename)
+
+	fmt.Println("Looking up file: ", filename, " with id: ", id, "at node: ", owner)
+}
+
+// Find whether id is in range [currentNode.id, successor.id).
+func ownerOfIdIsSuccessor(id id) bool {
+	// Find whether y is in range [x, x), only true if y = x.
+	fmt.Println(id)
+	fmt.Println(currentNode.id)
+	fmt.Println(successor.id)
+
+	// ALGORITM BARA PSEUDOKODDDDDDDD. Whether this node's successor is the owner of id.
+	// EXEMPEL:
+	// currentNode = 5, successorNode = 8, id = 6 => true
+	// currentNode = 5, successorNode = 8, id = 4 => false
+	// currentNode = 5, successorNode = 8, id = 9 => false
+	// currentNode = 5, successorNode = 8, id = 5 => true
+	// currentNode = 5, successorNode = 8, id = 8 => false
+	// efter (ink) 5 och innan 8
+	// id >= currentNode.id && id < successorNode.id
+	// Case 1 is only if currentNode < successorNode
+	if currentNode.id.Cmp(successor.id) == -1 {
+		return (id.Cmp(currentNode.id) == 1 || id.Cmp(currentNode.id) == 0) && id.Cmp(successor.id) == -1
+	} else { // Other case.
+		return (id.Cmp(currentNode.id) == 1 || id.Cmp(currentNode.id) == 0) || id.Cmp(successor.id) == -1
+	}
+	//
+	// currentNode = 8, successorNode = 5, id = 6 => false
+	// currentNode = 8, successorNode = 5, id = 4 => true
+	// currentNode = 8, successorNode = 5, id = 9 => true
+	// efter (ink) 8 eller innan 5
+	// id >= currentNode.id || id < successorNode.id
+	//
+	// currentNode = 5, successorNode = 5, id = 5 => true
+	// currentNode = 5, successorNode = 5, id = 3 => true
+	// currentNode = 5, successorNode = 5, id = 8 => true
+	// efter (ink) 5 eller innan 5
+	// id >= currentNode.id || id < successorNode.id
+
+	// return (id.Cmp(currentNode.id) == 1 || id.Cmp(currentNode.id) == 0) && id.Cmp(successor.id) == -1
+}
+
+// Find the node responsable for storing `id`.
+func findSuccessor(id id) (bool, node) {
+	if ownerOfIdIsSuccessor(id) {
+		return true, successor
+	}
+	return false, successor
+}
+
+func findSuccessorIteratively(id *big.Int) node {
+	nextNode := currentNode
+	for {
+		println("Looping finding successor iteratively")
+		found, nextNode := nodeInterface.FindSuccessor(nextNode, id)
+		if found {
+			return nextNode
+		}
+	}
+}
+
+func storeFile(filename string) {
+	id := getId(filename)
+
+	owner := findSuccessorIteratively(id)
+
+	// file := nodeInterface.RequestFile(owner, filename)
+
+	// hej.txt -> oasethu134n5hntsoaehu
+	// Hash the filename to get the fileid.
+	// owner := findSuccessor(id)
+	// owner.requestFile(filename)
+
+	fmt.Println("Storing file: ", filename, " with id: ", id, "at node: ", owner)
+	// // id := getId(filename)
+	// // ownerNode := findSuccessorIteratively(id)
+	// // Read file from disk and make post request to ownerNode.
+
+	// fmt.Println("Storing file: ", filename)
+}
+
+// Hashes the name, filename or node address, to get the id in the ring.
+func getId(name string) *big.Int {
+	return hash(name)
+}
+
+func hash(elt string) *big.Int {
+	hasher := sha1.New()
+	hasher.Write([]byte(elt))
+	return new(big.Int).SetBytes(hasher.Sum(nil))
+}
+
+func printState() {
+	fmt.Println("Printing state")
+}
+
+func listenForConnections(host string, port int) {
+	server, error := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
+
+	if error != nil {
+		fmt.Println("Failed to listen to connection ", error.Error())
+		os.Exit(1)
+	}
+	defer server.Close()
+
+	http.HandleFunc("/ping", handlePing)
+	http.HandleFunc("/findSuccessor", handleFindSuccessor)
+	http.HandleFunc("/getPredeccessor", handleGetPredeccessor)
+	http.HandleFunc("/notify", handleNotify)
+	http.Serve(server, nil)
+}
+
+func handlePing(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(200)
+}
+
+type FindSucessorHttpRequest struct {
+	id string
+}
+
+func handleFindSuccessor(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Handling find successor")
+	id := req.URL.Query().Get("id")
+	fmt.Println("Handling find successor, id is ", id)
+
+	// var successorHttpRequest FindSucessorHttpRequest
+	// err := json.NewDecoder(req.Body).Decode(&successorHttpRequest)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// String to bigint?
+	n := new(big.Int)
+	n, ok := n.SetString(id, 10)
+	if !ok {
+		panic("Not ok")
+	}
+
+	fmt.Println("Before")
+	found, node := findSuccessor(n)
+	fmt.Println("After")
+	response := FindSuccessorHttpResponse{
+		SuccessorAdress: node.address,
+		FoundSuccessor:  found,
+	}
+	fmt.Println("Response is ", response)
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("No error")
+	}
+}
+
+func handleGetPredeccessor(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Handling get predeccessor")
+	// id := req.URL.Query().Get("id")
+	// fmt.Println("Handling find successor, id is ", id)
+
+	// var successorHttpRequest FindSucessorHttpRequest
+	// err := json.NewDecoder(req.Body).Decode(&successorHttpRequest)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// String to bigint?
+	// n := new(big.Int)
+	// n, ok := n.SetString(id, 10)
+	// if !ok {
+	// 	panic("Not ok")
+	// }
+
+	fmt.Println("Before")
+
+	fmt.Println("After")
+	var response GetPredeccessorHttpResponse
+	if predecessor != nil {
+		response = GetPredeccessorHttpResponse{
+			address: predecessor.address,
+		}
+	} else {
+		response = GetPredeccessorHttpResponse{}
+	}
+	fmt.Println("Response is ", response)
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("No error")
+	}
+}
+
+func handleNotify(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Handling notify")
+	// id := req.URL.Query().Get("id")
+	// fmt.Println("Handling find successor, id is ", id)
+
+	// var successorHttpRequest FindSucessorHttpRequest
+	// err := json.NewDecoder(req.Body).Decode(&successorHttpRequest)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// String to bigint?
+	// n := new(big.Int)
+	// n, ok := n.SetString(id, 10)
+	// if !ok {
+	// 	panic("Not ok")
+	// }
+
+	// Parse the address.
+	address := "todo"
+	node := createNode(address)
+	notify(node)
+
+	w.WriteHeader(200)
+	// fmt.Println("Before")
+
+	// fmt.Println("After")
+	// var response GetPredeccessorHttpResponse
+	// if predecessor != nil {
+	// 	response = GetPredeccessorHttpResponse{
+	// 		address: predecessor.address,
+	// 	}
+	// } else {
+	// 	response = GetPredeccessorHttpResponse{}
+	// }
+	// fmt.Println("Response is ", response)
+
+	// err := json.NewEncoder(w).Encode(response)
+	// if err != nil {
+	// 	panic(err)
+	// } else {
+	// 	fmt.Println("No error")
+	// }
+}
